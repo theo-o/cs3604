30f6872010eb5dc9160262e14fcc70a9980d3560
bbtrevor <trevorbagbey@gmail.com>
2022-11-05 21:43:01 -0400

    Updates to authentication and uploading

--- src/components/UploadSection.js
+++ src/components/UploadSection.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import { API, Storage } from "aws-amplify";
+import React, { useState, useEffect } from 'react';
+import { API, Storage, Auth } from "aws-amplify";
 
 function UploadSection() {
 
     const [currFile, setCurrFile] = useState();
     const [fileIsSelected, setFileIsSelected] = useState(false);
+    const [isAuthorized, setIsAuthorized] = useState(false);
+    const [groups, setGroups] = useState();
 
     const handleChange = (e) => {
         setCurrFile(e.target.files[0]);
@@ -14,22 +16,60 @@ function UploadSection() {
     async function handleSubmit() {
         if (fileIsSelected) {
             try {
-                await Storage.put(currFile.name, currFile, {});
+                Storage.configure({
+                    customPrefix: {
+                        public: 'public/casestudies/'
+                    }
+                });
+                await Storage.put(currFile.name, currFile, {
+                    contentType: currFile.type
+                });
             } 
             catch (err) {
                 console.log("Error uploading given file: ", err);
             }
+            // TODO: Update archives
             setFileIsSelected(false);
         }
 
     }
 
+    async function authUser() {
+        try {
+            const data = await Auth.currentUserPoolUser();
+            const g = data.signInUserSession.accessToken.payload["cognito:groups"];
+            setGroups(g);
+            const type = process.env.STUDENT_AUTH;
+            if (groups && groups.indexOf(type) !== -1) {
+                setIsAuthorized(true);
+            } else {
+                setIsAuthorized(false);
+            }
+
+        } catch (err) {
+            console.log("error: ", err);
+            setIsAuthorized(false);
+        }
+    }
+
+    useEffect(() => {
+        authUser();
+    }, [])
+
     return (
         <div>
-            <input type='file' name='file' onChange={handleChange} />
+        {isAuthorized ? (
             <div>
-                <button onClick={handleSubmit}>Submit File</button>
+                <input type='file' name='file' onChange={handleChange} />
+                <div>
+                    <button onClick={handleSubmit}>Submit File</button>
+                </div>
             </div>
+            ) : (
+                <div>
+                    <h1>You are not authorized to access this page.</h1>
+                </div>
+            )}
         </div>
     )
 }
