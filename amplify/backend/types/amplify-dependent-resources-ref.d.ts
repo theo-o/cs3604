3485c60efacfe1640bb7ce9660abd6819fde1b59
Theo Ouzhinski <touzhinski@gmail.com>
2023-03-01 11:20:00 -0500

    Update amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,45 +1,46 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "collectionarchives": {
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "function": {
-        "iawav2658176f3PostConfirmation": {
-            "Name": "string",
-            "Arn": "string",
-            "LambdaExecutionRole": "string",
-            "Region": "string"
-        }
-    },
-    "auth": {
-        "iawav2658176f3": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "AppClientSecret": "string"
-        },
-        "userPoolGroups": {
-            "AdminGroupRole": "string",
-            "EditorGroupRole": "string",
-            "SiteAdminGroupRole": "string",
-            "IAWAGroupRole": "string",
-            "hokiesGroupRole": "string",
-            "SWVAGroupRole": "string",
-            "podcastsGroupRole": "string",
-            "DefaultGroupRole": "string",
-            "federatedGroupRole": "string",
-            "testGroupRole": "string"
-        }
-    },
-    "storage": {
-        "Collectionmap": {
-            "BucketName": "string",
-            "Region": "string"
-        }
+  "api": {
+    "collectionarchives": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
     }
+  },
+  "auth": {
+    "iawav2658176f3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "AppClientSecret": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdminGroupRole": "string",
+      "DefaultGroupRole": "string",
+      "EditorGroupRole": "string",
+      "IAWAGroupRole": "string",
+      "SWVAGroupRole": "string",
+      "SiteAdminGroupRole": "string",
+      "federatedGroupRole": "string",
+      "hokiesGroupRole": "string",
+      "podcastsGroupRole": "string",
+      "testGroupRole": "string"
+    }
+  },
+  "function": {
+    "iawav2658176f3PostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "Collectionmap": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
 }
