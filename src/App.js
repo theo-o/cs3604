dc5e25e6c8a8a6e26ffe17a09ff1d1623a95b24a
Theo Ouzhinski <theoo@vt.edu>
2023-03-27 23:10:15 -0400

    Add missing imports

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Amplify, Auth } from "aws-amplify";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./lib/ScrollToTop";
